84841abcd4c72353a50bc63b0f8eb7ae572be1ff
Omkar Mokashi <mokashi.omkara@gmail.com>
2024-11-26 20:04:47 +0530

    feat: Added trip count logic based on strategy

---
+++ TrainRoutes/DistanceBasedTripStrategy.cs
@@ -0,0 +1,9 @@
+namespace TrainRoutes;
+
+public class DistanceBasedTripStrategy : ITripStrategy
+{
+    public int GetTripCount(Graph graph, string startTown, string endTown, int conditionValue = Int32.MaxValue)
+    {
+        throw new NotImplementedException();
+    }
+}
