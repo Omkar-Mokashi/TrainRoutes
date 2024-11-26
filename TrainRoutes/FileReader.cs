27b75e506cc1ec19b42215964a3a08940c2a2766
Omkar Mokashi <mokashi.omkara@gmail.com>
2024-11-26 16:06:41 +0530

    Added function description for file reader

--- TrainRoutes/FileReader.cs
+++ TrainRoutes/FileReader.cs
@@ -2,6 +2,11 @@ namespace TrainRoutes;
 
 public class FileReader
 {
+    /// <summary>
+    /// The file reader function for our specific file format
+    /// </summary>
+    /// <param name="filePath">Input file path location in the project</param>
+    /// <returns>The graph structure as provided by the input file</returns>
     public static Graph ReadGraphFromFile(string filePath)
     {
         var graph = new Graph();
