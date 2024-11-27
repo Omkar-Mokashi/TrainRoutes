cbd86c87b58906c18a7f9fcc23fe106ff5523349
Omkar Mokashi <mokashi.omkara@gmail.com>
2024-11-27 17:08:56 +0530

    Added tests for a few clases

--- TrainRoutes/Strategies/RouteStrategy/DirectRouteStrategy.cs
+++ TrainRoutes/Strategies/RouteStrategy/DirectRouteStrategy.cs
@@ -2,7 +2,7 @@ namespace TrainRoutes.Strategies.RouteStrategy;
 
 public class DirectRouteStrategy : IRouteStrategy
 {
-    public int CalculateRoute(Graph graph, string startTown, string endTown, int maxStops = Int32.MaxValue)
+    public int CalculateRoute(Graph graph, string startTown, string endTown)
     {
         var routes = graph.GetRoutesFromTown(startTown).FirstOrDefault(x => x.ToTown == endTown);
         return routes?.Distance ?? throw new ArgumentException($"There is no direct route from {startTown} to {endTown}");
